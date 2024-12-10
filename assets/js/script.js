f4eb2dd4b44777556070dbbedb15086ca8b725ff
Rafa <contatopessoalrafaelamancio@gmail.com>
2024-12-10 16:46:24 -0300

    Dark Theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,29 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
@@ -57,8 +80,8 @@ function scrollTop(){
 window.addEventListener('scroll', scrollTop)
 
 
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selected_Theme = localStorage.getItem('selected-theme')
+const selected_Icon = localStorage.getItem('selected-icon')
 
 
 const sr = ScrollReveal({
